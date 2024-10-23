e0df59afaabf40303e2b47e18f1403fe5cea15ed
Matt Hyeun <mhyeun@homepartners.tech>
2024-10-22 21:33:51 -0400

    Vertically center date and chip

--- src/components/BlogPreview/style.ts
+++ src/components/BlogPreview/style.ts
@@ -19,6 +19,7 @@ export const getBlogDateAndCategoryBoxStyle: () => ComponentProps<
 > = () => ({
   display: 'flex',
   mb: '0.5vh',
+  alignItems: 'center',
 });
 
 export const getBlogDateStyle: () => ComponentProps<
